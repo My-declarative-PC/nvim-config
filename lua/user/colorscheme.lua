eeb8e71e867e98136a24ce783c9edf75dd9fbde3
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-01-09 14:33:02 +0300

    удалил запиненые версии

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,12 +1,22 @@
 local M = {
-  "LunarVim/primer.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  commit = "b8d7e08eed9a61eb2f49b9196b01f7f2932735ff",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "primer_dark"
+    require("catppuccin").setup({
+        integrations = {
+            indent_blankline = {
+                enabled = true,
+                scope_color = "mauve", -- catppuccin color (eg. `lavender`) Default: text
+                colored_indent_levels = true,
+            },
+        },
+    })
+
+    vim.cmd.colorscheme "catppuccin"
 end
 
 return M
